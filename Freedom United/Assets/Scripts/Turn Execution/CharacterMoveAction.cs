8d8afe60ae8fadd30da8d959b56ef64cac7fff2c
Juliana Montes <julymontes495@gmail.com>
2021-12-16 09:49:52 +0100

    Made character movement relative to characters position when executing the action

--- Freedom United/Assets/Scripts/Turn Execution/CharacterMoveAction.cs
+++ Freedom United/Assets/Scripts/Turn Execution/CharacterMoveAction.cs
@@ -2,24 +2,24 @@ using UnityEngine;
 
 public class CharacterMoveAction : ExecutingAction
 {
+    private CharacterID movingCharacter;
     private Vector2Int finalPosition;
     private Vector2Int originPosition;
     private bool changeWithTeammate;
 
     public CharacterMoveAction(AllyAction scheduledAction) : base(scheduledAction)
     {
-        changeWithTeammate = BattleManager.Instance.CharacterManagement.GetCharacterInPosition(scheduledAction.position) != null;
-
         finalPosition = scheduledAction.position;
-        originPosition = BattleManager.Instance.CharacterManagement.Characters[scheduledAction.actionOwner].CurrentPosition;
+        movingCharacter = scheduledAction.actionOwner;
     }
 
     public override void Execute()
     {
+        originPosition = BattleManager.Instance.CharacterManagement.Characters[movingCharacter].CurrentPosition;
+        changeWithTeammate = BattleManager.Instance.CharacterManagement.GetCharacterInPosition(finalPosition) != null;
+
         if (changeWithTeammate)
-        {
             BattleManager.Instance.CharacterManagement.GetCharacterInPosition(finalPosition).MoveToPosition(originPosition);
-        }
 
         BattleManager.Instance.CharacterManagement.GetCharacterInPosition(originPosition).MoveToPosition(finalPosition);
 
